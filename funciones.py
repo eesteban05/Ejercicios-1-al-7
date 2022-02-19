03d3ff48caebb21848aa73bb6596c510c8f89efc
eesteban05 <99089352+eesteban05@users.noreply.github.com>
2022-02-19 13:31:20 -0600

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,21 @@
+# FUNCIÓN: Tarea a ejecutar
+# SINTAXIS
+# def Nombre_de_la_Función (Parámetros o Argumentos):
+
+def Suma(a, b): #Obtiene o recibe argumentos o parámetros
+    suma = a + b
+    return suma #return: retornar, Devolver o Regresar un valor
+
+def Resta(a, b):
+    return a - b
+
+def Multi(a,b):
+    return a * b
+
+def Divi(a,b):
+    return a / b
+
+print(f"La suma = {Suma(10 , 5)}")
+print(f"La suma = {Resta(10 , 5)}")
+print(f"La suma = {Multi(10 , 5)}")
+print(f"La suma = {Divi(10 , 5)}")
